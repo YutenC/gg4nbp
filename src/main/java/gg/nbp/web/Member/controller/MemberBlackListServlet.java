10ca172d1e5787d8fdb2f4325016980f27aaa179
YutenC <max875e6@gmail.com>
2023-06-24 14:43:05 +0800

    1.刪除刪減功能的Servlet

--- src/main/java/gg/nbp/web/Member/controller/MemberBlackListServlet.java
+++
@@ -1,8 +0,0 @@
-package gg.nbp.web.Member.controller;
-
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-
-@WebServlet("/html/member_blacklist")
-public class MemberBlackListServlet extends HttpServlet {
-}
