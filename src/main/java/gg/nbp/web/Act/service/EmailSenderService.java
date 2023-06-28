056138738210a9f99480101b4a586525dda82a1a
YutenC <max875e6@gmail.com>
2023-06-28 19:20:26 +0800

    1.更新porn檔設定 更新 javax mail 為 jakarta mail

--- src/main/java/gg/nbp/web/Act/service/EmailSenderService.java
+++ src/main/java/gg/nbp/web/Act/service/EmailSenderService.java
@@ -2,6 +2,7 @@ package gg.nbp.web.Act.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
