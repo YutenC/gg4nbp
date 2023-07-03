cfb70df4237b3532ffa80d5939d9d7fbfb97402d
WuSeagal <wuseagal@gmail.com>
2023-07-03 11:00:48 +0800

    更新 離職沒有權限的filter 砍掉一切檢舉相關的東西 會員：詳細資料部分密碼欄位拔除

--- src/main/java/gg/nbp/web/power_of_manager/filter/PowerSecondHandFilter.java
+++ src/main/java/gg/nbp/web/power_of_manager/filter/PowerSecondHandFilter.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.core.annotation.Order;
 
+import gg.nbp.web.Manager.entity.Manager;
 import gg.nbp.web.power.entity.Power;
 import gg.nbp.web.power_of_manager.entity.Power_of_Manager;
 import jakarta.servlet.Filter;
@@ -48,32 +49,35 @@ public class PowerSecondHandFilter extends HttpFilter implements Filter{
 		HttpServletResponse resp= (HttpServletResponse) response;
 		
 		// get Session
-//		HttpSession session= req.getSession();
+		HttpSession session= req.getSession();
 		// 判斷登入
 		
-//		List<Power> powerList= (List<Power>) session.getAttribute("powerList");
-//		Optional<Integer> powerMngIdOpt = powerList.stream()
-//		        .filter(power -> "二手商城管理員".equals(power.getPower_name()))
-//		        .map(Power::getPower_id)
-//		        .findFirst();
-//	    int powerMngId = powerMngIdOpt.get();
+		List<Power> powerList= (List<Power>) session.getAttribute("powerList");
+		Optional<Integer> powerMngIdOpt = powerList.stream()
+		        .filter(power -> "二手商城管理員".equals(power.getPower_name()))
+		        .map(Power::getPower_id)
+		        .findFirst();
+	    int powerMngId = powerMngIdOpt.get();
 		
-//		List<Power_of_Manager> LoggedPomList= (List<Power_of_Manager>) session.getAttribute("loggedPomList");
+	    List<Power_of_Manager> LoggedPomList= (List<Power_of_Manager>) session.getAttribute("loggedPomList");
+		Manager manager= (Manager) session.getAttribute("manager");
 		
-//		boolean hasPower = LoggedPomList.stream()
-//		        .anyMatch(pom -> pom.getPower_id() == powerMngId);
+		boolean hasPower = LoggedPomList.stream()
+		        .anyMatch(pom -> pom.getPower_id() == powerMngId);
 		
-//		if (!(hasPower)) {
-//			String redirectUrl = req.getRequestURI();
+		boolean isResign= (manager.getIs_working()== 0);
+		
+		if ((!(hasPower)) || isResign) {
+			String redirectUrl = req.getRequestURI();
 			
 			// 将需要跳转的页面路径存储在会话变量中
-//			req.getSession().setAttribute("redirectUrl", redirectUrl);
+			req.getSession().setAttribute("redirectUrl", redirectUrl);
 			
-//			resp.sendRedirect(req.getContextPath()+ "/manager_noPower.html"); 
-//			return;
-//		}else {
+			resp.sendRedirect(req.getContextPath()+ "/manager_noPower.html"); 
+			return;
+		}else {
 			chain.doFilter(request, response);
-//		}
+		}
 		
 	}
 }
