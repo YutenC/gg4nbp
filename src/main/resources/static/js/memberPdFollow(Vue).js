37f06050a912034a3eab063678f7efb2369b652f
kazupende <kazupendetw@gmail.com>
2023-07-07 10:46:41 +0800

    add member product follow page

--- src/main/resources/static/js/memberPdFollow(Vue).js
+++ src/main/resources/static/js/memberPdFollow(Vue).js
@@ -5,6 +5,42 @@ const detailUnit = Vue.createApp({
         }
     },
     methods: {
+        goProduct: function (location, otherDetail) {
+            sessionStorage.setItem('currentShopProductDetail_id', otherDetail);
+            window.location.replace(projectFolder + '/' + location);
+        },
+        addCart: function (action, id) {
+            let transObj = { productId: id, buyAmount: 1 };
+            axios({
+                method: "Post",
+                url: projectFolder + '/' + "ShoppingList",
+                // withCredentials: true,
+                // crossDomain: true,
+                params: {
+                    demand: "addOneShoppingList",
+                    transObj: JSON.stringify(transObj),
+                }
+            })
+                .then(function (response) {
+                    let result = response.data;
+                    if (result != null && result.redirect) {
+                        window.location.href = projectFolder + "/member_login.html";
+                    }
+                    else {
+                        if (action == 1) {
+                            window.location.href = "../member/shoppingCart(Vue).html";
+                        }
+                    }
+
+                    Toast.fire({
+                        icon: 'success',
+                        title: '已加入購物車'
+                    });
+                })
+                .catch(function (e) {
+                    console.log("addCart error " + e);
+                });
+        },
         remove(index, id) {
             Swal.fire({
                 title: '確定刪除?',
@@ -17,16 +53,42 @@ const detailUnit = Vue.createApp({
             }).then(function (result) {
                 if (result.isConfirmed) {
                     detailUnit.mypick.splice(index, 1);
-                    axios.get('/Five_NBP.gg/FollowList?delPdId=' + id)
-                        .then(res => console.log(res.data))
-                        .catch(err => console.log(err))
+                    axios({
+                        method: "Get",
+                        url: projectFolder + "shopDispatcher/addFollow",
+                        params: {
+                            id: id,
+                            redirectUrl: "http://localhost:8080/gg4nbp/shop/shopProductDetail.html"
+                        }
+                    })
+                        .then(function (value) {
+                            let result = value.data;
+
+                            if (result.state.toLowerCase() === "redirect") {
+                                console.log(result.msg);
+                                window.location.href = result.msg;
+                            }
+                            else if (result.state.toLowerCase() === "ok") {
+                                this.mypick[index].follow = result.content;
+                            }
+
+                            if (this.mypick[index].follow === 0) {
+                                Toast.fire({
+                                    icon: 'error',
+                                    title: '已從追蹤清單移除'
+                                })
+                            }
+                        })
+                        .catch(function (e) {
+                            console.log("addFollow error " + e);
+                        });
                 }
             });
         }
     },
     created() {
-        axios.get('/Five_NBP.gg/FollowList?getAll=true')
+        axios.get(projectFolder + '/shopDispatcher/getFollowByMember')
             .then(res => this.mypick = res.data)
-            .catch(err => console.log(err));
+            .catch(error => console.log(error))
     }
 }).mount('.detailUnit');
